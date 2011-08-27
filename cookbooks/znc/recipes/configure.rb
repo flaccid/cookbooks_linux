ce3159d39604f93a848d500be125a5322d97777d
Chris Fordham <chris@xhost.com.au>
2011-08-27 19:01:53 +1000

    Add new files.

---
+++ cookbooks/znc/recipes/configure.rb
@@ -0,0 +1,25 @@
+# Cookbook Name:: znc
+# Recipe:: configure
+#
+# Copyright 2011, Chris Fordham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# render znc.conf
+template "#{node['znc']['data_dir']}/configs/znc.conf" do
+  source "znc.conf.erb"
+  mode 0600
+  owner node['znc']['user']
+  group node['znc']['group']
+  notifies :start, "service[znc]", :immediately
+end
---
+++ cookbooks/znc/recipes/generate_cert.rb
@@ -0,0 +1,30 @@
+# Cookbook Name:: znc
+# Recipe:: generate_cert
+#
+# Copyright 2011, Chris Fordham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+bash "generate-pem" do
+  cwd node['znc']['data_dir']
+  code <<-EOH
+  umask 077
+  openssl genrsa 2048 > znc.key
+  openssl req -subj /C=US/ST=Several/L=Locality/O=Example/OU=Operations/CN=#{node['fqdn']}/emailAddress=znc@#{node['fqdn']} \
+   -new -x509 -nodes -sha1 -days 3650 -key znc.key > znc.crt
+  cat znc.key znc.crt > znc.pem
+  EOH
+  user node['znc']['user']
+  group node['znc']['grouip']
+  creates "#{node['znc']['data_dir']}/znc.pem"
+end
