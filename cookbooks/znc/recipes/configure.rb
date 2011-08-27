8ee46941814a6adfdeb950754139e02c4589bce1
Chris Fordham <chris@xhost.com.au>
2011-08-27 22:00:10 +1000

    Add default users and modules.

--- cookbooks/znc/recipes/configure.rb
+++ cookbooks/znc/recipes/configure.rb
@@ -17,11 +17,21 @@
 
 service "znc"
 
+if !Chef::Config.solo
+  users = search(:users, 'groups:znc')
+else
+  users = node[:znc][:users]
+end
+
 # render znc.conf
 template "#{node.znc.data_dir}/configs/znc.conf" do
   source "znc.conf.erb"
   mode 0600
   owner node.znc.user
   group node.znc.group
+  variables(
+    :users => users,  
+    :modules => node.znc.modules
+  )
   notifies :start, "service[znc]", :immediately
 end
