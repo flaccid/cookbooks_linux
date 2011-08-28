33f43c54d2b22fb1c1f30f3ad1d4b75b186f6837
Chris Fordham <chris@xhost.com.au>
2011-08-28 10:41:42 +1000

    Add znc service stanza.

--- cookbooks/znc/recipes/configure.rb
+++ cookbooks/znc/recipes/configure.rb
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+service "znc"
+
 include_recipe("znc::install_service")
 
 package "coreutils" do
