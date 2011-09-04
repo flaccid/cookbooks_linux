5a8605dcf4bacd38b1013bbf4a784e56761fe252
Chris Fordham <chris@xhost.com.au>
2011-09-04 12:45:48 +1000

    Minor re-factor and use grep for screen -ls.

--- cookbooks/znc/recipes/makeconf_interactive_screen.rb
+++ cookbooks/znc/recipes/makeconf_interactive_screen.rb
@@ -21,8 +21,8 @@
 log "Starting detached screen session for #{node.znc.user}."
 
 execute "znc_makeconf_interactive" do
-  # when screen -ls is run at the end, if no screens exist a non-zero exit will be triggered
-  command "sudo -u #{node.znc.user} screen -S znc -t znc -d -m -c /dev/null -- sh -c 'znc -f --makeconf; exec $SHELL'; echo ; echo '==> screen sessions for #{node.znc.user}:'; sudo -u #{node.znc.user} screen -ls"
+  # when screen -ls | grep is run at the end, if no screens exist a non-zero exit will be triggered
+  command "sudo -u #{node.znc.user} screen -S znc -t znc -d -m -c /dev/null -- sh -c 'znc -f --makeconf; exec $SHELL'; echo '==> screen sessions for #{node.znc.user}:'; screen -ls | grep znc"
 end
 
-log "==> Now, to start the interactive configuration, switch user to '#{node.znc.user}' and run, 'screen -r -S znc'."
+log "==> Now, to start the interactive configuration, switch user to '#{node.znc.user}' and run, screen -r -S znc"
